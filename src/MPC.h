fb88403eaecbb3ef73601becbd7806c6b550cb63
shahbhatti <shahbhatti@gmail.com>
2018-02-20 02:03:37 -0700

    initial commit

---
+++ src/MPC.h
@@ -0,0 +1,32 @@
+#ifndef MPC_H
+#define MPC_H
+
+#include <vector>
+#include "Eigen-3.3/Eigen/Core"
+
+using namespace std;
+
+struct Solution {
+
+    vector<double> X;
+    vector<double> Y;
+    vector<double> Psi;
+    vector<double> V;
+    vector<double> Cte;
+    vector<double> EPsi;
+    vector<double> Delta;
+    vector<double> A;
+};
+
+class MPC {
+ public:
+  MPC();
+
+  virtual ~MPC();
+
+  // Solve the model given an initial state and polynomial coefficients.
+  // Return the first actuatotions.
+  Solution Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+};
+
+#endif /* MPC_H */
